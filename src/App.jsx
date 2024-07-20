7fbe74341d45fd4e21fd724a6dec38164390db80
Qendresa-Potoku <94839915+Qendresa-Potoku@users.noreply.github.com>
2024-07-20 15:35:51 +0200

    job listings

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
     </div>
   );
 }
